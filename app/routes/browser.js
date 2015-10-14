4035a7e16e4e329ba877bd0423b2e86e2fac7973
Mauricio Mena <j.menamen@gmail.com>
2015-10-14 16:25:56 -0300

    Add owl-carousel components and styles

--- app/routes/browser.js
+++ app/routes/browser.js
@@ -6,5 +6,16 @@ export default Ember.Route.extend({
       playlists: this.database.findAll('featured-playlist'),
       genres: this.database.findAll('genre')
     });
+  },
+  actions:{
+    goToNext: function() {
+      var owl = $('.owl-carousel');
+      owl.trigger('next.owl.carousel');
+    },
+
+    goToPrevious: function() {
+      var owl = $('.owl-carousel');
+      owl.trigger('prev.owl.carousel', [300]);
+    }
   }
 });
