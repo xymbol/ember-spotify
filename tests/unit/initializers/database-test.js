ffe68b1ff56fec3368a91507ddf1c24ea8f9aecd
Adrian Mugnolo <adrian@mugnolo.com>
2015-10-29 17:05:18 -0300

    Fix initializer arity

--- tests/unit/initializers/database-test.js
+++ tests/unit/initializers/database-test.js
@@ -2,13 +2,12 @@ import Ember from 'ember';
 import { initialize } from '../../../initializers/database';
 import { module, test } from 'qunit';
 
-var registry, application;
+var application;
 
 module('Unit | Initializer | database', {
   beforeEach: function() {
     Ember.run(function() {
       application = Ember.Application.create();
-      registry = application.registry;
       application.deferReadiness();
     });
   }
@@ -16,7 +15,7 @@ module('Unit | Initializer | database', {
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  initialize(registry, application);
+  initialize(application);
 
   // you would normally confirm the results of the initializer here
   assert.ok(application.hasRegistration('service:database'));
