b435aaab52b3cd31d2ceb1c50a7b509153c21e44
Adrian Mugnolo <adrian@mugnolo.com>
2015-10-29 17:05:48 -0300

    Add data models

---
+++ tests/unit/models/song-test.js
@@ -0,0 +1,12 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('song', 'Unit | Model | song', {
+  // Specify the other units that are required for this test.
+  needs: ['model:playlist']
+});
+
+test('it exists', function(assert) {
+  var model = this.subject();
+  // var store = this.store();
+  assert.ok(!!model);
+});
